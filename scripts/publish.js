4a44d06d05c3532464d40e9443678fd941c66b00
Alex Vizcaino <avizcaino@uxland.es>
2020-01-30 07:01:17 +0100

    chore: publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,13 +1,21 @@
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const spawn = require('child_process').spawn;
 const branchName = require('current-git-branch');
 
 const prereleaseBranches = ['alpha', 'beta'];
 const isPrereleaseBranch = branch => prereleaseBranches.indexOf(branch) > -1;
 
 const publish = async () => {
-  if (isPrereleaseBranch(branchName())) await exec(`lerna version --conventional-prerelease`);
-  else await exec(`lerna version --conventional-commits`);
+  let cmd;
+  if (isPrereleaseBranch(branchName())) cmd = spawn('lerna', ['version', '--conventional-prerelease']);
+  else cmd = spawn('lerna', ['version', '--conventional-commits']);
+
+  try {
+    cmd.stdout && cmd.stdout.on('data', data => console.log(`stdout: ${data.toString()}`));
+    cmd.stderr && cmd.stderr.on('data', data => console.log(`stderr: ${data.toString()}`));
+    cmd.exit && cmd.exit.on('data', data => console.log(`child process exited with code: ${data.toString()}`));
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 publish();
