d1842dcefcbe48458721c222b365becf7b5f022a
Alex Vizcaino <avizcaino@uxland.es>
2020-02-12 11:28:30 +0100

    feat: set-modules action

--- packages/prism/src/user/actions.ts
+++ packages/prism/src/user/actions.ts
@@ -1,5 +1,6 @@
-import { createActionThunk } from '@uxland/redux';
-import { FETCH_USER_ACTION, LOGIN_USER_ACTION } from './reducer';
+import { createAction, createActionThunk } from '@uxland/redux';
+import { FETCH_USER_ACTION, LOGIN_USER_ACTION, ModuleInfo, SET_MODULES } from './reducer';
 
 export const fetchUserAction: (action: Function) => any = action => createActionThunk(FETCH_USER_ACTION, action);
 export const loginUserAction: (action: Function) => any = action => createActionThunk(LOGIN_USER_ACTION, action);
+export const setModulesAction = createAction<ModuleInfo[]>(SET_MODULES);
