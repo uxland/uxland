c23cf368d5504e652615027144deef2183efcb49
Alex Vizcaino <avizcaino@uxland.es>
2020-08-28 23:58:45 +0200

    fix: solves issue with lint-staged

--- packages/project-tools/lint/lint-staged.config.js
+++ packages/project-tools/lint/lint-staged.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    "*.ts": ["eslint -c config/lint/.eslintrc.js", "prettier --write"]
-}
+  '*.ts': ['eslint -c node_modules/@uxland/project-tools/lint/.eslintrc.js', 'prettier --write'],
+};
