05a5579d98c7dc42425b4b47981738e52b08fa25
Alex Vizcaino <avizcaino@uxland.es>
2021-01-12 13:08:04 +0100

    fix: cache in fuse and update watcher roots

--- packages/project-tools/fuse/fuse.ts
+++ packages/project-tools/fuse/fuse.ts
@@ -1,4 +1,5 @@
 import { fusebox, pluginSass, pluginLink, pluginCSS } from "fuse-box";
+import { IDevServerProps } from "fuse-box/devServer/devServerProps";
 import { join } from "path";
 // import { runUpdateSimulation } from './simulate';
 require("dotenv").config();
@@ -9,45 +10,64 @@ console.log("Workspace Root:", workspaceRoot);
 // set AUTOMOD to true to automatically update some files to trigger the watcher
 // runUpdateSimulation(!!process.env.AUTOMOD);
 
+let STUB = 1;
+/**
+ * Fuse configuration
+ * @interface FuseConfig
+ * @memberof ProjectTools
+ * @since v1.0.0
+ * @property {string} entry Demo entry point
+ * @property {string} baseStyles Base scss styles path
+ * @property {string} webIndex Demo entry html
+ * @property {boolean|Object} devServer Run server configuration
+ * @property {string=} publicPath Public path
+ * @property {string[]=} workspaces Workspaces ids
+ * @property {*=} env Environment variables
+ */
+interface FuseConfig {
+  entry: string;
+  baseStyles: string;
+  webIndex: string;
+  devServer: boolean | IDevServerProps;
+  publicPath?: string;
+  workspaces?: string[];
+  env?: any;
+}
+STUB = 1;
+
 /**
  * Fuse base constructor
- * @param {Object} config Fuse configuration
- * @param {string} config.entry Demo entry point
- * @param {string} config.baseStyles Base scss styles path
- * @param {string} config.webIndex Demo entry html
- * @param {boolean} config.devServer Run server
- * @param {string=} config.publicPath Public path
- * @param {string[]=} config.workspaces Workspaces ids
- * @param {*=} config.env Environment variables
+ * @memberof ProjectTools
+ * @function
+ * @name fuse
+ * @param {ProjectTools.FuseConfig} config Fuse configuration
+ 
  */
-export const fuse = ({
-  entry,
-  baseStyles,
-  webIndex,
-  devServer,
-  publicPath = "/",
-  workspaces = ["packages"],
-  env = {},
-}) =>
-  fusebox({
-    cache: false,
+export function fuse(config: FuseConfig) {
+  return fusebox({
+    cache: true,
     compilerOptions: {
       tsConfig: "tsconfig.json",
     },
-    devServer,
+    devServer: config.devServer,
     hmr: true,
-    entry,
-    env,
+    entry: config.entry,
+    env: config.env || {},
     target: "browser",
     watcher: {
-      root: [workspaceRoot],
+      root: (config.workspaces || ["packages"]).map((w) =>
+        join(workspaceRoot, w)
+      ),
     },
     stylesheet: {
       macros: {
         "~": join(__dirname, "../../../"),
       },
-      autoImport: baseStyles
-        ? workspaces.map((w) => ({ file: baseStyles, capture: `${w}/*/src` }))
+      autoImport: config.baseStyles
+        ? (config.workspaces || ["packages"]).map((w) => ({
+            file: config.baseStyles,
+            capture: `${w}/*/src`,
+          }))
         : undefined,
     },
     plugins: [
@@ -56,9 +76,13 @@ export const fuse = ({
       pluginLink(/.+\.png/, { useDefault: true }),
       pluginLink(/.+\.svg/, { useDefault: true }),
     ],
-    webIndex: { template: webIndex, publicPath },
+    webIndex: {
+      template: config.webIndex,
+      publicPath: config.publicPath || "/",
+    },
     resources: {
-      resourcePublicRoot: `${publicPath}resources/`,
+      resourcePublicRoot: `${config.publicPath || "/"}resources/`,
       resourceFolder: `resources/`,
     },
   });
+}
