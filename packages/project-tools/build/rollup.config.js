759640f5a5f98b4ed8227beb9132b2f5680e0029
Alex Vizcaino <avizcaino@uxland.es>
2021-03-10 14:20:14 +0100

    build: disable rollup banner

--- packages/project-tools/build/rollup.config.js
+++ packages/project-tools/build/rollup.config.js
@@ -1,37 +1,39 @@
-'use strict';
+"use strict";
 
-const babel = require('rollup-plugin-babel');
+const babel = require("rollup-plugin-babel");
 // uglify handles only es5 code, so this also acts as smoke test against shipping es2015+ syntax
-const uglify = require('rollup-plugin-uglify').uglify;
+const uglify = require("rollup-plugin-uglify").uglify;
 
-const buildConfig = pkg => {
+const buildConfig = (pkg) => {
   const banner = `// ${pkg.description} v${pkg.version}
 // ${pkg.homepage}
 // (c) 2020-${new Date().getFullYear()} UXLand
 // UXLand libraries may be freely distributed under the MIT license.\n`;
 
-  const input = 'lib/index.js';
+  const input = "lib/index.js";
 
   const config = {
     input: input,
     output: {
-      format: 'umd',
-      name: 'FunctionalUtilities',
-      exports: 'named',
-      banner: banner
+      format: "umd",
+      name: pkg.name,
+      exports: "named",
+      // banner: banner
     },
-    plugins: [babel({ presets: [['@babel/preset-env', { targets: { ie: '11' } }]] })]
+    plugins: [
+      babel({ presets: [["@babel/preset-env", { targets: { ie: "11" } }]] }),
+    ],
   };
 
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === "production") {
     config.plugins.push(
       uglify({
         compress: {
           pure_getters: true,
           unsafe: true,
-          unsafe_comps: true
+          unsafe_comps: true,
         },
-        warnings: false
+        warnings: false,
       })
     );
   }
