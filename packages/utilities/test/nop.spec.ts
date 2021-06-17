79108a45f7b01e0feb8ac059dfbdd7982d6d27f2
Alex Vizcaino <avizcaino@uxland.es>
2021-06-17 10:29:02 +0200

    fix(utilities): specific ramda imports and web-test-runner

--- packages/utilities/test/nop.spec.ts
+++ packages/utilities/test/nop.spec.ts
@@ -1,7 +1,8 @@
+import {expect} from '@open-wc/testing';
 import {nop} from '../nop';
 
 describe('when invoking `nop` method', () => {
   it('should do nothing', () => {
-    expect(nop()).toBeUndefined();
+    expect(nop()).to.be.undefined;
   });
 });
