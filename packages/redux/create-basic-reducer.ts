cf80d0d7a5aaa61dbe1a1618c782d1b8900fe990
Alex Vizcaino <avizcaino@uxland.es>
2021-07-29 09:47:51 +0200

    fix(redux): accept empty string as defValue in createBasicReducer

--- packages/redux/create-basic-reducer.ts
+++ packages/redux/create-basic-reducer.ts
@@ -15,6 +15,7 @@
  * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+import {isNotNil} from '@uxland/ramda-extensions/is-not-nil';
 import set from 'ramda/es/set';
 import {Lens} from 'ramda/tools';
 import {Reducer} from 'redux';
@@ -53,5 +54,5 @@ export const createBasicReducer: <T = any>(
   options?: BasicOptions<T>
 ) => Reducer<T> =
   (actionName, options = {defValue: null}) =>
-  (state = options.defValue || ({} as any), action: Action): any =>
+  (state = isNotNil(options.defValue) ? options.defValue : ({} as any), action: Action): any =>
     action.type === actionName ? setState(state, action, options.path) : state;
